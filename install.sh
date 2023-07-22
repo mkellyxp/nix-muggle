8e596fc2a02d0aa9d084dba954ff9e4dd9ca4466
Mike Kelly <mike@membervault.co>
2023-07-22 09:57:07 -0700

    install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+sudo nixos-rebuild switch
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+flatpak install -y flathub com.google.Chrome
+flatpak install -y flathub org.libreoffice.LibreOffice
+flatpak install -y flathub us.zoom.Zoom
+flatpak install -y mint-y-dark-blue
+
+reboot
